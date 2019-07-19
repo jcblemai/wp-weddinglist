6c0bd2ca34ce4e4cba41bc0c95e41b839a0d7ac7
chadi <joseph.lemaitre@epfl.ch>
2019-07-18 20:21:55 -0700

    Remove description on product. YOu might want to keep that as it's not very nice now :/

--- wpneotemplate/woocommerce/basic/include/loop/author.php
+++ wpneotemplate/woocommerce/basic/include/loop/author.php
@@ -4,6 +4,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 $author_name = wpneo_crowdfunding_get_author_name();
 ?>
-<p class="wpneo-author"><?php _e('by','wp-crowdfunding'); ?> 
-	<a href="<?php echo wpneo_crowdfunding_campaign_listing_by_author_url( get_the_author_meta( 'user_login' ) ); ?>"><?php echo $author_name; ?></a>
-</p>
+
--- wpneotemplate/woocommerce/basic/include/loop/description.php
+++ wpneotemplate/woocommerce/basic/include/loop/description.php
@@ -5,5 +5,3 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 $col_num = get_option('number_of_words_show_in_listing_description', 130);
 ?>
-
-<p class="wpneo-short-description"><?php echo WPNEOCF()->limit_word_text(strip_tags(get_the_content()), $col_num); ?></p>
