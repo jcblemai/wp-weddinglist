7df3fa14afa646b81a3ff23399c8146871e3603e
chadi <joseph.lemaitre@epfl.ch>
2019-07-18 20:13:25 -0700

    No more location by  changeing the /basic/include/loop/ file

--- wpneotemplate/woocommerce/basic/include/loop/location.php
+++ wpneotemplate/woocommerce/basic/include/loop/location.php
@@ -4,7 +4,3 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 $location = wpneo_crowdfunding_get_campaigns_location();
 ?>
-<div class="wpneo-location">
-    <i class="wpneo-icon wpneo-icon-location"></i>
-    <div class="wpneo-meta-desc"><?php echo $location; ?></div>
-</div>
